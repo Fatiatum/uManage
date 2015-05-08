c7e383d7fc1f3f0606147b731f54c63f39ae16df
Fatiatum <ei12030@fe.up.pt>
2015-05-08 22:12:34 +0100

    small changes

--- actions/users/login.php
+++ actions/users/login.php
@@ -15,7 +15,7 @@
   if (isLoginCorrect($username, $password)) {
     $_SESSION['username'] = $username;
     //$_SESSION['success_messages'][] = 'Login successful';
-    header('Location: pages/users/profile.php');
+    header('Location: {$BASE_URL}pages/users/profile.php');
   } else {
     $_SESSION['error_messages'][] = 'Login failed';
   }
