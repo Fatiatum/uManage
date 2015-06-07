72d0700d6d8c653b09b5bcf469647501df6b2ff9
AnaRitaFerreira <ei12052@fe.up.pt>
2015-06-07 14:45:39 +0100

    proj corrigido

--- database/project.php
+++ database/project.php
@@ -1,35 +1,5 @@
 <?php
-/*
-function getUserProjects($username) {
-  
-  global $conn;
-  $username = $_SESSION['username'];
 
-  $userId = $conn->prepare("SELECT users_id FROM users WHERE username= :username");
-  $userId->bindParam(':username', $username);
-  $userId->execute();
-  $rows = $userId->fetch(PDO::FETCH_NUM);
-  
-  $stmt = $conn->prepare("SELECT * FROM project WHERE coordenator_id = :id");
-  $stmt->bindParam(':id', $rows[0]);
-  $stmt->execute();
-  return $stmt->fetch();
-  
-}*/
-
-
-
-
-/*
-function getAllProjects() {
-  global $conn;
-  $stmt = $conn->prepare("SELECT *
-    FROM project
-    ORDER BY dataCriacao DESC");
-  $stmt->execute();
-  return $stmt->fetchAll();
-}
-*/
 function createProject($name, $descp, $visibility){
   global $conn;
   
@@ -87,7 +57,7 @@ function getTasksfromTList($name, $tasklist){
   global $conn;
   $stmt = $conn->prepare("SELECT * FROM task,(SELECT project_id FROM project WHERE name=:name) AS idproj WHERE task.project_id=idproj.project_id AND task.task_list_id!=NULL ORDER BY task.task_list_id");
   $stmt->bindParam(':name', $name);
-  tmt->bindParam(':tasklist', $tasklist);
+  $stmt->bindParam(':tasklist', $tasklist);
   $stmt->execute();
   return $stmt->fetchAll();
 }
